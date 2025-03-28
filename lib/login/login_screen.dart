cdd7233f7bacbfdedbbba7f3602a92009b8262b0
Karoluk2a4s <100285386+Karoluk2a4s@users.noreply.github.com>
2025-03-28 16:31:21 +0200

    login page - started creating email sectioonn

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -7,54 +7,67 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFA500),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-                child: Container(
-                  color: Colors.transparent,
-                  child: //logo
-                  Center(
-                    child: Icon(
-                      Icons.lock,
-                      size: 150,
-                    ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+                color: Colors.transparent,
+                child: //logo
+                Center(
+                  child: const Icon(
+                    Icons.lock,
+                    size: 150,
                   ),
-                )
-            ),
-            Expanded(
-              flex: 2,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40)
-                    ),
-                    color: Color(0xFFADD8E6),
+                ),
+              )
+          ),
+          Expanded(
+            flex: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)
                   ),
-                  child: Column(
-                    children: [
-        
-                      //el. paštas
-        
-                      //slaptažodis
-        
-                      //pamiršau slaptažodį
-        
-                      //prisijungti mygtukas
-        
-                      //arba
-        
-                      //google ir apple prisijungimas
-        
-                      //naujas vartotojas? registruotis
-                    ],
-        
-                  ),
-                )
-            )
-          ],
-        ),
+                  color: Color(0xFFADD8E6),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    //el. paštas
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey)
+                          ),
+                          fillColor: Color(0xFFFFA500),
+                          filled: true
+                        ),
+                      ),
+                    )
+
+                    //slaptažodis
+
+                    //pamiršau slaptažodį
+
+                    //prisijungti mygtukas
+
+                    //arba
+
+                    //google ir apple prisijungimas
+
+                    //naujas vartotojas? registruotis
+                  ],
+
+                ),
+              )
+          )
+        ],
       ),
     );
   }
